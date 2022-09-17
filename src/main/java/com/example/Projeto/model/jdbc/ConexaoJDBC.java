390f02e0dcb5521d78ca5e7e48a6861890e7a42f
Eduardo-Ozika <eduardoozika.ti@gmail.com>
2022-09-16 21:00:31 -0300

    Interface de conexão caso queira trocar o banco de dados

---
+++ src/main/java/com/example/Projeto/model/jdbc/ConexaoJDBC.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.Projeto.model.jdbc;
+
+/**
+ *
+ * @author eduar
+ */
+import java.sql.Connection;
+
+public interface ConexaoJDBC {
+
+    public Connection criarConexao();
+
+}
